517258852c56c7824c9e5ac3e0d93f33a2b6a560
Thiesa <thiesa.c@gmail.com>
2023-09-28 08:52:53 -0400

    feat: testing /login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,26 +1,45 @@
-// describe('Endpoint /login', () => {
-//   //   let authService: AuthService;
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
 
-//   //   beforeEach(() => {
-//   //     authService = new AuthService();
-//   //   })
+import { app } from '../app';
+import { Response } from 'superagent';
+import { Sequelize } from 'sequelize';
 
-//   //   afterEach(() => {
-//   //     sinon.restore();
-//   //   });
+chai.use(chaiHttp);
 
-//   //   describe('POST /login', () => {
-//   //     it('should login with valid credentials and return a token', (done) => {
-//   //       chai
-//   //         .request(app)
-//   //         .post('/login')
-//   //         .send({ email: 'valid@example.com', password: 'validpassword' })
-//   //         .end(async (err, res) => {
-//   //           // expect(err).to.be.null;
-//   //           expect(res).to.have.status(200);
-//   //           // expect(res.body).to.have.property('token');
-//   //           done();
-//   //         })
-//   //     })
-//   //   })
-//   // })
+const { expect } = chai;
+
+afterEach(() => {
+  sinon.restore();
+});
+
+describe('LOGIN', () => {
+  describe('endpoint /login', () => {
+    beforeEach(async () => {
+      const sequelize = new Sequelize({
+        dialect: 'mysql',
+        host: 'db',
+        port: 3306,
+        username: 'root',
+        password:  '123456',
+        database: 'TRYBE_FUTEBOL_CLUBE',
+      });
+      await sequelize.sync({ force: true });
+    })
+    it('should return a valid token when valid credentials are provided', async function () {
+      
+      const res = await chai
+        .request(app)
+        .post('/login')
+        .send({
+          email: 'admin@admin.com',
+          password: 'secret_admin',
+        });
+
+      expect(res).to.have.status(200);
+
+    })
+  })
+})
